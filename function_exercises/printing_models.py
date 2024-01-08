becccb0253c8d8c12b3399b9e87f299e619b4b13
rukynas <rukynas08@gmail.com>
2024-01-08 23:50:14 +0100

    adding

---
+++ function_exercises/printing_models.py
@@ -0,0 +1,10 @@
+# printing_functions.py
+
+def print_models(models):
+    for model in models:
+        print(f"Printing model: {model}")
+
+def show_completed_models(completed_models):
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
